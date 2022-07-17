8498f9c35b88e77b56901254dda17fd1a737968d
ladynskaya95 <feverrr27@gmail.com>
2022-07-17 19:09:44 +0300

    adding api

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 let apiKey = "217e1884eaeda1d000c71ab70e625169"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kiev&appid=${apiKey}&units=metric`;
+
+function displayTemperature(response) {
+  document.querySelector("#temperature-degree").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#current-condition").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+axios.get(apiUrl).then(displayTemperature);
